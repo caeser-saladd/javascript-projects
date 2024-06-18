92f2f1c8a0d61669f7a7db312ff88f389b08b9de
caeser-saladd <csgutreuter@gmail.com>
2024-06-17 20:26:17 -0500

    Studio 1 all done

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -13,3 +13,74 @@
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+
+
+let date = "Monday 2019-03-18"
+let time = "10:05:34 AM"
+let astronautCount = 7
+let astronautStatus = "ready"
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount * averageAstronautMassKg
+let fuelMassKg = 760000
+let shuttleMassKg = 74842.31
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = "100%"
+let weatherStatus = "clear"
+let preparedForLiftOff = true
+
+
+if (astronautCount > 7){
+    preparedForLiftOff = false
+} else {
+    preparedForLiftOff = true
+}
+
+if (astronautStatus === "ready"){
+    preparedForLiftOff = true
+} else {
+    preparedForLiftOff = false
+}
+
+if (totalMassKg < maximumMassLimit){
+    preparedForLiftOff = true
+} else {
+    preparedForLiftOff = false
+}
+
+if (fuelTempCelsius < -300 || fuelTempCelsius > -150){
+    preparedForLiftOff = false
+} else {
+    preparedForLiftOff = true
+}
+
+if (fuelLevel === "100%"){
+    preparedForLiftOff = true
+} else {
+    preparedForLiftOff = false
+}
+
+if (weatherStatus === "clear"){
+    preparedForLiftOff = true
+} else {
+    preparedForLiftOff = false
+}
+
+
+if (preparedForLiftOff = true){
+    console.log("Date:", date)
+    console.log("Time:", time)
+    console.log("Astronaut Count:", astronautCount)
+    console.log("Crew Mass:", crewMassKg)
+    console.log("Fuel Mass:", fuelMassKg)
+    console.log("Shuttle Mass:", shuttleMassKg)
+    console.log("Total Mass:", totalMassKg)
+    console.log("Fule Temperature:", fuelTempCelsius)
+    console.log("Weather Status", weatherStatus)
+    console.log("Have a safe trip astronauts!")
+} else {
+    console.log("CONDITIONS ARE NOT MET, LAUNCH OPERATIONS ABORTED")
+}
